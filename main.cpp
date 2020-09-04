5c3e5f223f206d4b298b27b9c3ad3469ef8101b4
repl.it user <replituser@example.com>
2020-09-04 22:00:12 +0000

    fixing merge issues p2

--- main.cpp
+++ main.cpp
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #include "include/Compiler.h"
 #include "include/ExecutionEngine.h"
 using namespace std;
@@ -11,20 +10,4 @@ int main() {
 
   ExecutionEngine e(pt);
   e.run();
-=======
-#include "include/Scanner.h"
-using namespace std;
-
-int main() {
-  Scanner s("Test.gs");
-  Token t;
-  for (;;) {
-    t = s.scanToken();
-    if(t.type == TOKEN_EOF) {
-      exit(0);
-    }
-    cout << t.start << endl;
-  }
->>>>>>> cf9083e6d6959b7c53418be31fd4fefcd4713077
-  return 0;
 }
